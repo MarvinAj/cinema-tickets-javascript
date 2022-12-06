52cfa1f29c345f7dc79777cb5dcfc717171cb87c
Marvin Wereko <marvinuky@gmail.com>
2022-12-06 20:30:28 +0000

    Changed invalid route method name

--- src/pairtest/routes/InvalidRoute.js
+++ src/pairtest/routes/InvalidRoute.js
@@ -3,6 +3,6 @@ import invalidPathcontroller from '../controllers/InvalidPathController.js';
 
 const invalidRouter = express.Router();
 
-errorRouter.get('/invalid', invalidPathcontroller);
+invalidRouter.get('/invalid', invalidPathcontroller);
 
 export default invalidRouter;
